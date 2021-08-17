3ae1be167d78f9beb54d9efc01d1df216e858702
heanbian <heanbian@heanbian.com>
2021-08-17 19:52:55 +0800

    1

---
+++ src/main/java/com/heanbian/block/util/DesensitizationUtils.java
@@ -0,0 +1,22 @@
+package com.heanbian.block.util;
+
+public final class DesensitizationUtils {
+
+	private static final String PHONE_REGEX = "(86){0,1}1\\d{10}";
+
+	public static String de(String value) {
+		if (value == null || "".equals(value)) {
+			return "";
+		}
+		if (value.contains("@")) {
+			return value.replaceAll("(\\w+)\\w{4}@(\\w+)", "$1****@$2");
+		}
+		if (value.matches(PHONE_REGEX)) {
+			return value.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
+		}
+		return value;
+	}
+
+	private DesensitizationUtils() {
+	}
+}
