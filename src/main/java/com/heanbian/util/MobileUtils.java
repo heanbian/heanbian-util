d4aa1cf5d6e818c49ebbd8f58a31c811f41cdfc0
heanbian <heanbian@heanbian.com>
2025-04-09 16:44:18 +0800

    1
    1

--- src/main/java/com/heanbian/util/MobileUtils.java
+++ src/main/java/com/heanbian/util/MobileUtils.java
@@ -1,19 +1,17 @@
 package com.heanbian.util;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class MobileUtils {
 
-	private static final String PHONE_NUMBER_PATTERN = "^(\\+86)?1[3-9]\\d{9}$";
-	private static final Pattern PATTERN = Pattern.compile(PHONE_NUMBER_PATTERN);
+	private static final String PHONE_NUMBER_REGEX = "^(\\+86)?1[3-9]\\d{9}$";
+	private static final Pattern PATTERN = Pattern.compile(PHONE_NUMBER_REGEX);
 
 	public static boolean isValid(String mobile) {
 		if (mobile == null) {
 			return false;
 		}
-		Matcher matcher = PATTERN.matcher(mobile);
-		return matcher.matches();
+		return PATTERN.matcher(mobile).matches();
 	}
 
 }
