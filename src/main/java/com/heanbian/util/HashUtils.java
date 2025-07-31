fc8e69dc1c15224d9d534c9927eaa3a062743132
heanbian <heanbian@heanbian.com>
2025-07-31 18:40:31 +0800

    1
    1

--- src/main/java/com/heanbian/util/HashUtils.java
+++ src/main/java/com/heanbian/util/HashUtils.java
@@ -1,52 +1,26 @@
 package com.heanbian.util;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.HexFormat;
+import java.util.zip.CRC32;
 
 public final class HashUtils {
 
-	public static String sha3(String input) {
-		return sha3(input, SHA3_TYPE.SHA3_224);
+	private static final String BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	public static String generateShortHash(String input) {
+		CRC32 crc32 = new CRC32();
+		crc32.update(input.getBytes());
+		long checksum = crc32.getValue();
+		return toBase62(checksum);
 	}
 
-	public static String sha3(String input, SHA3_TYPE sha3Type) {
-		return sha3(input, sha3Type.getAlg());
-	}
-
-	private static String sha3(String input, String alg) {
-		MessageDigest md;
-		try {
-			md = MessageDigest.getInstance(alg);
-			byte[] buf = md.digest(input.getBytes(StandardCharsets.UTF_8));
-			return HexFormat.of().formatHex(buf);
-		} catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException(alg, e);
-		}
-	}
-
-	static enum SHA3_TYPE {
-
-		SHA3_224("SHA3-224"), //
-		SHA3_256("SHA3-256"), //
-		SHA3_384("SHA3-384"), //
-		SHA3_512("SHA3-512");
-
-		private final String alg;
-
-		SHA3_TYPE() {
-			this("SHA3-224");
-		}
-
-		SHA3_TYPE(String alg) {
-			this.alg = alg;
-		}
-
-		public String getAlg() {
-			return alg;
-		}
-
+	private static String toBase62(long value) {
+		StringBuilder sb = new StringBuilder();
+		do {
+			int index = (int) (value % 62);
+			sb.append(BASE62.charAt(index));
+			value /= 62;
+		} while (value > 0);
+		return sb.reverse().toString();
 	}
 
 }
